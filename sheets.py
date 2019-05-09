1f62bcc3666061dc720f8715850934b2d6e0f883
rhaeyx <francis@rhaey.com>
2019-05-09 16:29:43 +0800

    added sleep for scrolling

--- sheets.py
+++ sheets.py
@@ -1,4 +1,5 @@
 from pyautogui import *
+import time
 
 filename = 'data_google.txt'
 
@@ -11,26 +12,27 @@ for data in data:
     splitted_data = data.split(' ')
     ready_data.append(splitted_data)
 
-print(ready_data) 
-
 # Typing Data
 
-base_coord = (300, 215)
+# EDIT THIS
+base_coord = (275, 200)
+
 
 # Model #
 click(base_coord)
 
 for data in ready_data:
-    typewrite(data[1])
+    typewrite(data[-2])
     typewrite('\n')
 print('Model Done')
 
 # Oregon #
 scroll(100000)
+time.sleep(2)
 click(base_coord)
 
 typewrite('\t')
 for data in ready_data:
-    typewrite(data[2])
+    typewrite(data[-1])
     typewrite('\n\t')
 print('Oregon Done')
